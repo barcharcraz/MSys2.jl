7adc579e78665a04e056fef10d4953e80034cea7
Charles <bartoc@umich.edu>
2016-05-27 21:07:36 -0400

    added version note

--- src/mingw64_bindeps.jl
+++ src/mingw64_bindeps.jl
@@ -1,10 +1,9 @@
 using BinDeps
-import BinDeps: PackageManager, can_use, package_available,
-        available_version, generate_steps, LibraryDependency, pkg_name,
-        libdir
+import BinDeps: PackageManager, can_use, package_available, available_version,
+    libdir, generate_steps, LibraryDependency, provider, provides, pkg_name
 type Mingw64 <: PackageManager
   package :: AbstractString
-  Mingw64(package) = new("mingw-w64-x86_64-" * package)
+  Mingw64(package :: AbstractString) = new("mingw-w64-x86_64-" * package)
 end
 
 can_use(::Type{Mingw64}) = OS_NAME == :Windows
@@ -14,5 +13,9 @@ pkg_name(m :: Mingw64) = m.package
 libdir(m :: Mingw64, dep) = joinpath(Pkg.dir("MSys2"), "deps", "msys64", "mingw64", "bin")
 
 function generate_steps(dep :: LibraryDependency, m :: Mingw64, opts)
+  if get(opts,:force_rebuild,false)
+    error("Will not force yum to rebuild dependency \"$(dep.name)\".\n"*
+        "Please make any necessary adjustments manually (This might just be a version upgrade)")
+  end
   () -> install(m.package)
 end
